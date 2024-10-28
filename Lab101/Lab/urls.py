a23652aa74ef98d272233bbc6ae6c80218fc0c3d
alhanoufALLUHAYDDAN <alhanoufkhalaf1@gmail.com>
2024-10-29 01:32:55 +0300

    i just finished the HTML and CSS sections and created three pages

---
+++ Lab101/Lab/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = "Lab"
+
+urlpatterns = [
+    path("", views.home_view, name= "home_view"),
+    path("html/introduction/", views.thingsIlove_view, name="thingsIlove_view"),
+    path("css/introduction/", views.thingsIlove_styled_view, name="thingsIlove_styled_view"),
+]
