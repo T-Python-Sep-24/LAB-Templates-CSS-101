17e1f977415f7c1cabca349f4920dce187734bce
Eyad <eyalghamdi@outlook.com>
2024-10-29 13:53:06 +0300

    add index nav bar

--- PracticeHtmlCss/myapplication/urls.py
+++ PracticeHtmlCss/myapplication/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 app_name = "myapplication"
 urlpatterns = [
+    path("",views.index_view,name="index_view"),
     path("introduction",views.html_intro_view,name="html_intro_view"),
     path("introduction_css",views.css_intro_view,name="css_intro_view"),
-    path("",views.article,name="article"),
+    path("article",views.article,name="article"),
     path("cv",views.cv,name="cv"),
 ]
--- PracticeHtmlCss/myapplication/views.py
+++ PracticeHtmlCss/myapplication/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
-
+def index_view(request:HttpRequest):
+    return render(request,"myapplication/index.html")
 
 def html_intro_view(request:HttpRequest):
     return render(request,"myapplication/introduction.html")
@@ -14,7 +15,6 @@ def article(request:HttpRequest):
 
 def cv(request:HttpRequest):
     return render(request, "myapplication/cv.html")
-
 
 
 
