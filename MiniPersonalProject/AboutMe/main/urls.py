41a99a92a9f47e4be6f1190665a3fb96de2d27bd
MarwahDesigner90 <marwaty666@gmail.com>
2024-10-30 08:15:43 +0300

    final version of mini project created

--- MiniPersonalProject/AboutMe/main/urls.py
+++ MiniPersonalProject/AboutMe/main/urls.py
@@ -5,6 +5,9 @@ app_name ="main"
 
 urlpatterns = [
 
-    path('/index/', views.introduction_view, name="introduction_view")
-
+    path('html/Hobbies/', views.Hobbies_view , name='Hobbies_view'),
+    path('introduction/', views.introduction_view, name='introduction_view'), 
+    path('article/ai/', views.article_view ,name='article_view'),
+    path('careers/cv/', views.cv_view ,name='cv_view'),
 ]
+
--- MiniPersonalProject/AboutMe/tailwind.config.js
+++ MiniPersonalProject/AboutMe/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './templates/**/*.html', // Scans all HTML files in the templates folder
+    './**/templates/**/*.html', // Scans all HTML files in the templates folder
     './src/**/*.js',        // If you have JavaScript files in the src folder
   ],
   theme: {
