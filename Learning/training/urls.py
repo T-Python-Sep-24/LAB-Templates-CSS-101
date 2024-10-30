a1f266551e8cc4c44fcd11e90c2ef581c1c94ca1
Ali <almaghamsiali@gmail.com>
2024-10-30 06:53:54 +0300

    lab solution

--- Learning/training/urls.py
+++ Learning/training/urls.py
@@ -8,5 +8,5 @@ urlpatterns=[
     path("html/introduction/",views.html_view,name='html_view'),
     path("css/introduction/",views.css_view,name='css_view'),
     path("article/ai/",views.article_view,name='article_view'),
-    path("careers/cv/",views.cv_view,name='cv_ciew'),
+    path("careers/cv/",views.cv_view,name='cv_view'),
 ]
