0d9631daafdb0ff3e1d87e0dcdbe9c6cf2f1bafa
Areej Shareefi <aomsh1420@gmail.com>
2024-10-30 07:19:44 +0300

    final commit for mini projects

--- CSSandHTMLlearningJourney/mini_projects/views.py
+++ CSSandHTMLlearningJourney/mini_projects/views.py
@@ -5,3 +5,15 @@ from django.http import HttpRequest,HttpResponse
 # Create your views here.
 def html_view(request:HttpRequest):
     return render(request,"mini_projects/html_page.html")
+
+def css_view(request):
+    return render(request,"mini_projects/css_page.html")
+
+def ai_view(request):
+    return render(request, "mini_projects/ai.html")
+
+def cv_view(request):
+    return render(request, "mini_projects/cv.html")
+
+def cv_work_view(request):
+    return render(request, "mini_projects/cv_work.html")
